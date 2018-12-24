f15fe68c350769c61a815081ec398b828e9e59be
RicciLiao <riccix@163.com>
2018-12-24 17:47:21 +0800

    sysnc ASL -Logo

--- src/main/java/com/ricciliao/bsm/controller/MdCon.java
+++ src/main/java/com/ricciliao/bsm/controller/MdCon.java
@@ -3,9 +3,7 @@ package com.ricciliao.bsm.controller;
 import com.ricciliao.bsm.common.Constants;
 import com.ricciliao.bsm.pojo.UserInfoPo;
 import net.sf.json.JSONObject;
-import org.apache.catalina.User;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpRequest;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,23 +12,19 @@ import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.multipart.MultipartHttpServletRequest;
 import sun.misc.BASE64Encoder;
 
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.*;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.nio.file.Files;
-import java.util.HashMap;
-import java.util.Map;
 
 @Controller
 @RequestMapping("/Md")
 public class MdCon {
 
-    @Autowired
-    private HttpServletRequest g_request;
+    @Value("${spring.web.image.server.url}")
+    private String imageSer;
 
     @PostMapping("/upLoadImg")
     @ResponseBody
@@ -41,7 +35,7 @@ public class MdCon {
         HttpSession curSession = null;
         UserInfoPo poFromSession = null;
         String ajaxResult = null;
-        MultipartHttpServletRequest mreq = null;
+        MultipartHttpServletRequest mReq = null;
         try {
             if (request instanceof MultipartHttpServletRequest) {
 
@@ -49,8 +43,8 @@ public class MdCon {
                 poFromSession = (UserInfoPo) curSession.getAttribute(curSession.getId());
                 if (poFromSession != null) {
 
-                    mreq = (MultipartHttpServletRequest) request;
-                    MultipartFile fileFromReq = mreq.getFile("editormd-image-file");
+                    mReq = (MultipartHttpServletRequest) request;
+                    MultipartFile fileFromReq = mReq.getFile("editormd-image-file");
                     System.out.println(fileFromReq.getContentType());
 
                     mreqType = fileFromReq.getContentType().split("/");
@@ -61,8 +55,7 @@ public class MdCon {
                         strImgToB64 = base64Encoder.encode(fileFromReq.getBytes());
 
                         response.setContentType("text/html;charset=utf-8");
-                        String http = "http://localhost:8090/bsmImgSer/writeImg";
-                        URL url = new URL(http);
+                        URL url = new URL(imageSer);
                         HttpURLConnection httpurlconnection = (HttpURLConnection) url.openConnection();
                         httpurlconnection.setDoOutput(true);
                         httpurlconnection.setRequestMethod("POST");
@@ -73,6 +66,7 @@ public class MdCon {
                         writer.close();
 
                         int responseCode = httpurlconnection.getResponseCode();
+                        System.out.println("Response Code is: " + responseCode);
                         if (responseCode == HttpURLConnection.HTTP_OK) {
                             InputStream urlStream = httpurlconnection.getInputStream();
                             BufferedReader reader = new BufferedReader(new InputStreamReader(urlStream));
@@ -81,19 +75,12 @@ public class MdCon {
                             while ((line = reader.readLine()) != null) {
                                 tLine += line;
                             }
-                            System.out.println(tLine);
-
                             ajaxResult = JSONObject.fromObject(tLine).toString();
-
                         } else {
-                            System.out.println("ERR" + responseCode);
+                            ajaxResult = JSONObject.fromObject("{\"success\" : 0,\"message\" : \"上传失败！\"}").toString();
                         }
                     }
-
-
                 }
-
-
             }
         } catch (Exception e) {
             e.printStackTrace();
