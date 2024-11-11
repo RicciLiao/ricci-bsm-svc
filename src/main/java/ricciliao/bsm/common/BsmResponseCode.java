d159cfd394b3f8b941a4832dade28f3d5b32e572
ricciliao <riccix@163.com>
2024-11-11 09:44:50 +0800

    refactor: 1. common exception; 2. common response code.

--- src/main/java/ricciliao/bsm/common/BsmResponseCode.java
+++ src/main/java/ricciliao/bsm/common/BsmResponseCode.java
@@ -6,17 +6,17 @@ public enum BsmResponseCode implements ResponseVoCode {
 
     POST_SIGN_UP_SEND_POST_EXISTED_EMAIL(50, "This Email has been registered, please use another Email or you can sign in directly.");
 
-    BsmResponseCode(int code, String message) {
-        this.code = code;
+    BsmResponseCode(int id, String message) {
+        this.id = id;
         this.message = message;
     }
 
-    private final long code;
+    private final long id;
     private final String message;
 
     @Override
     public long getId() {
-        return code;
+        return id;
     }
 
     @Override
