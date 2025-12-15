56655907fed0039ca45bb36a6960854d6ed9d513
ricciliao <riccix@163.com>
2025-12-15 16:57:13 +0800

    feat: use RestClient instead of RestTemplate.

--- src/main/java/ricciliao/bsm/configuration/BsmBeanConfig.java
+++ src/main/java/ricciliao/bsm/configuration/BsmBeanConfig.java
@@ -1,30 +1,17 @@
 package ricciliao.bsm.configuration;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
-import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
-import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import ricciliao.bsm.filter.BsmFilter;
-import ricciliao.x.component.payload.response.ResponseHttpMessageConverter;
 import ricciliao.x.log.MdcSupportFilter;
 
 @Configuration
 @ComponentScan(basePackages = "ricciliao.x.starter.common")
 public class BsmBeanConfig implements WebMvcConfigurer {
-
-    private ObjectMapper objectMapper;
-
-    @Autowired
-    public void setObjectMapper(ObjectMapper objectMapper) {
-        this.objectMapper = objectMapper;
-    }
 
     @Bean
     public FilterRegistrationBean<BsmFilter> httpServletWrapperFilter() {
@@ -47,17 +34,6 @@ public class BsmBeanConfig implements WebMvcConfigurer {
         registrationBean.setName("mdcSupportFilter");
 
         return registrationBean;
-    }
-
-    @Bean
-    public RestTemplate messageRestTemplate() {
-
-        return new RestTemplateBuilder()
-                .messageConverters(
-                        new ResponseHttpMessageConverter(objectMapper),
-                        new MappingJackson2HttpMessageConverter(objectMapper)
-                )
-                .build();
     }
 
 }
